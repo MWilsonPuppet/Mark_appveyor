73a455016b2a0a2b399e9ea66463b7ebcdb4acc6
Mark Wilson <mark.wilson@puppetlabs.com>
2018-05-22 15:41:08 +0100

    Adding puppet to the path

--- puppet-configure.ps1
+++ puppet-configure.ps1
@@ -1,14 +1,19 @@
 . C:\projects\marktest\windows-env.ps1
 
-Write-Output "Installing Puppet Agent..."
+Write-Output "Downloading Puppet Agent..."
 if ("$ARCH" -eq "x86") {
   $global:PuppetMSIUrl = "https://downloads.puppetlabs.com/windows/puppet-agent-x86-latest.msi"
 } else {
   $global:PuppetMSIUrl = "https://downloads.puppetlabs.com/windows/puppet-agent-x64-latest.msi"
 }
 
+Write-Output "Installing Puppet Agent..."
 # Install Puppet Agent
 Download-File "$PuppetMSIUrl" $PackerDownloads\puppet-agent.msi
 Start-Process -Wait "msiexec" -PassThru -NoNewWindow -ArgumentList "/i $PackerDownloads\puppet-agent.msi /qn /norestart PUPPET_AGENT_STARTUP_MODE=manual"
 Write-Output "Installed Puppet Agent..."
+$ENV:PATH="$ENV:PATH;C:\Program Files\Puppet Labs\Puppet\bin"
 echo $ENV:PATH
+
+Write-Output "Installing puppetlabs-docker module"
+puppet module install puppetlabs-docker
