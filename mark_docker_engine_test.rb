4fdc205dce4ebbaa90096af6d015a592f50f4b9e
Mark Wilson <mark.wilson@puppetlabs.com>
2018-06-04 10:48:06 +0100

    Adding rspect test

--- mark_docker_engine_test.rb
+++ mark_docker_engine_test.rb
@@ -3,6 +3,8 @@ require 'serverspec'
   set :backend, :cmd
   set :os, :family => 'windows'
 
-describe command('docker -ps') do
-  its(:exit_status) { should eq 1 }
+describe command('docker ps') do
+  its(:exit_status) { should eq 0 }
 end          
+
+system ("powershell -File C:\\projects\\marktest\\test.ps1")
